813d74d81c6f7f62ba7a6ac45467365a86a04be4
Molnár Balázs <molbazsa@gmail.com>
2022-11-15 16:14:05 +0100

    Bástya rajzolása

--- js/scenes/loading.scene.js
+++ js/scenes/loading.scene.js
@@ -14,8 +14,9 @@ loadingScene.preload = function () {
     this.load.image('Bkiralyno', 'assets/kiralyno_black.png');
     this.load.image('Wcsiko', 'assets/csiko_white.png');
     this.load.image('Bcsiko', 'assets/csiko_black.png');
-    this.load.image('Wbastya', 'assets/bastya_white.png');
-    this.load.image('Bbastya', 'assets/bastya_black.png');
+    // this.load.image('Wbastya', 'assets/bastya_white.png');
+    // this.load.image('Bbastya', 'assets/bastya_black.png');
+
     this.barW = 500;
     this.barH = 30;
     this.gameW = this.sys.game.config.width;
@@ -42,35 +43,114 @@ loadingScene.preload = function () {
 
 
 loadingScene.createPieces = function() {
-    const bParasztGraphics = this.make.graphics({x: 0, y: 0, add: false});
-    bParasztGraphics.lineStyle(30, 0xFFFFFF, 1.0);
-    bParasztGraphics.fillStyle(0x000000, 1.0);
-    bParasztGraphics.beginPath();
-    bParasztGraphics.arc(512, 952, 400, 0, Math.PI, true);
-    bParasztGraphics.closePath();
-    bParasztGraphics.fillPath();
-    bParasztGraphics.strokePath();
-    bParasztGraphics.fillCircle(512, 450, 230);
-    bParasztGraphics.strokePath();
-    bParasztGraphics.fillCircle(512, 190, 120);
-    bParasztGraphics.strokePath();
-    bParasztGraphics.generateTexture('Bparaszt', 1024, 1024);
+    let piece;
+
+    piece = this.newBlackPiece();
+    drawPiece(piece, 'paraszt');
+    piece.generateTexture('Bparaszt', 1024, 1024);
     this.newprogressbar();
-    const wParasztGraphics = this.make.graphics({x: 0, y: 0, add: false});
-    wParasztGraphics.lineStyle(30, 0x000000, 1.0);
-    wParasztGraphics.fillStyle(0xFFFFFF, 1.0);
-    wParasztGraphics.beginPath();
-    wParasztGraphics.arc(512, 952, 400, 0, Math.PI, true);
-    wParasztGraphics.closePath();
-    wParasztGraphics.fillPath();
-    wParasztGraphics.strokePath();
-    wParasztGraphics.fillCircle(512, 450, 230);
-    wParasztGraphics.strokePath();
-    wParasztGraphics.fillCircle(512, 190, 120);
-    wParasztGraphics.strokePath();
-    wParasztGraphics.generateTexture('Wparaszt', 1024, 1024);
+
+    piece = this.newWhitePiece();
+    drawPiece(piece, 'paraszt');
+    piece.generateTexture('Wparaszt', 1024, 1024);
     this.newprogressbar();
+
+    piece = this.newBlackPiece();
+    drawPiece(piece, 'bastya');
+    piece.generateTexture('Bbastya', 1024, 1024);
+    this.newprogressbar();
+
+    piece = this.newWhitePiece();
+    drawPiece(piece, 'bastya');
+    piece.generateTexture('Wbastya', 1024, 1024);
+    this.newprogressbar();
+};
+
+
+loadingScene.newBlackPiece = function() {
+    const BLACK_COLOR = 0x000000;
+    const WHITE_COLOR = 0xFFFFFF;
+
+    const piece = this.make.graphics({x: 0, y: 0, add: false});
+    piece.lineStyle(30, WHITE_COLOR, 1.0);
+    piece.fillStyle(BLACK_COLOR, 1.0);
+
+    return piece;
+};
+
+
+loadingScene.newWhitePiece = function() {
+    const BLACK_COLOR = 0x000000;
+    const WHITE_COLOR = 0xFFFFFF;
+
+    const piece = this.make.graphics({x: 0, y: 0, add: false});
+    piece.lineStyle(30, BLACK_COLOR, 1.0);
+    piece.fillStyle(WHITE_COLOR, 1.0);
+
+    return piece;
+};
+
+
+function drawPiece(piece, type) {
+    switch(type) {
+        case 'paraszt':
+            piece.beginPath();
+            piece.arc(511, 952, 400, 0, Math.PI, true);
+            piece.closePath();
+            piece.fillPath();
+            piece.strokePath();
+            piece.fillCircle(512, 450, 230);
+            piece.strokePath();
+            piece.fillCircle(512, 190, 120);
+            piece.strokePath();
+            break;
+
+        case 'bastya':
+            // draw bottom slabs
+            piece.fillRect(112, 872, 800, 80);
+            piece.strokeRect(112, 872, 800, 80);
+            piece.fillRect(192, 752, 640, 120);
+            piece.strokeRect(192, 752, 640, 120);
+            piece.beginPath();
+            piece.moveTo(192, 752);
+            piece.lineTo(232, 652);
+            piece.lineTo(792, 652);
+            piece.lineTo(832, 752);
+            piece.closePath();
+            piece.fillPath();
+            piece.strokePath();
+            // draw middle
+            piece.fillRect(232, 302, 560, 350);
+            piece.strokeRect(232, 302, 560, 350);
+            // draw top
+            piece.beginPath();
+            piece.moveTo(232, 302);
+            piece.lineTo(192, 202);
+            piece.lineTo(832, 202);
+            piece.lineTo(792, 302);
+            piece.closePath();
+            piece.fillPath();
+            piece.strokePath();
+            piece.beginPath();
+            piece.moveTo(192, 202);
+            piece.lineTo(192, 72);
+            piece.lineTo(272, 72);
+            piece.lineTo(272, 122);
+            piece.lineTo(472, 122);
+            piece.lineTo(472, 72);
+            piece.lineTo(552, 72);
+            piece.lineTo(552, 122);
+            piece.lineTo(752, 122);
+            piece.lineTo(752, 72);
+            piece.lineTo(832, 72);
+            piece.lineTo(832, 202);
+            piece.closePath();
+            piece.fillPath();
+            piece.strokePath();
+            break;
+    }
 }
+
 
 loadingScene.newprogressbar = function(){
     this.progressBar.clear();
